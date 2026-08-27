8a311b352726337a74a861f112b448f1fdbc0fdf
iKlassika <vash.yaro@gmail.com>
2026-08-27 19:32:39 +0300

    выаоплываполджывап

--- mods/SpaceMode.lua
+++
@@ -1,6 +0,0 @@
-function init()
-  api.set_gravity(2)
-  api.set_jump_force(14)
-  api.set_level_music("https://files.catbox.moe/06rdkm.mp3")
-  api.show_message("Лунный режим!", 4)
-end
--- mods/customsounds.lua
+++
@@ -1,6 +0,0 @@
-function init()
-  api.log("Мод загруден")
-  api.set_jump_sound("https://filebin.net/twnipjbdrpvpzi3n/8bit-synth-bounce-short.mp3")
-  api.set_damage_sound("https://files.catbox.moe/c8u714.mp3")
-  api.get_author()
-end
